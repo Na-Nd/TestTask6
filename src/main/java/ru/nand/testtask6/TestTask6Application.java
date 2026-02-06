80092832335c3a4f40c13556dcb5fede82c5be39
Na1Nd <gromovnkt04@gmail.com>
2026-02-06 03:20:02 +0300

    init commit

---
+++ src/main/java/ru/nand/testtask6/TestTask6Application.java
@@ -0,0 +1,13 @@
+package ru.nand.testtask6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTask6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTask6Application.class, args);
+    }
+
+}
